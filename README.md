db28b544052f626ee5cb8d6f5dfc8249f249f6a5
Faisal <fhoda@users.noreply.github.com>
2022-09-22 09:10:50 -0500

    sagemker processing added

--- README.md
+++ README.md
@@ -1,32 +1,24 @@
-# Databricks Data Science and Machine Learning Examples with Airflow
+# Sagemaker Batch Inference Examples with Airflow
 
 ## DAGs
 These DAGs are examples on how to use Airflow to orchestrate your ML Ops batch prediction pipelines with Sagemaker.
 
-1. **sagemaker-batch-inference.py** - Runs an end to end data ingest to model publishing pipeline with the following tasks:
-    - **etl (task group):** Performs ETL transformation to prepare data for prediction.
-      - **load_file (Astro SDK):** Loads file from S3 which 
-      - **transform_dataframe (Astro SDK)**: Prepares data for prediction
-      - **save_to_s3 (Astro SDK):** Writes data to S3
+1. **sagemaker-batch-inference-processing.py** - Runs an end to end data ingest to model publishing pipeline with the following tasks:
+    - **process_data (SagemakerProcessingOperator):** Performs ETL transformation to prepare data for prediction.
     - **get_latest_model_version (Python Decorated Task):** Get the latest model version from the Sagemaker Model Registry. Models are named with a distinct naming pattern `<model-name>-v<version number>`
-    - **predict (SageMakerTransformOperator):** 
-
-Note: For this DAG we place AWS credentials as environment variables and not as an Airflow connection. This is to simply avoid putting them in two places, since the API calls to Sagemaker or MLflow that don't use an Airflow operator cannot access those credentials from connections.
-
-
+    - **predict (SageMakerTransformOperator):**
 
 ## Requirements
 
 ### AWS Sagemaker
   - Have a pre-trained, registered, and created model. To do this you can use the Sagemaker notebook provided `include/helper/astronomer-sagemaker-blogpost.ipynb`.
-
+  - Build and push the image used in the SageMakerProcessing task to ECR
+    - All code for the image is in `include/helper/processing_job` and you can run the docker commands from that directory
+    - Directions on building and pushing https://docs.aws.amazon.com/AmazonECR/latest/userguide/docker-push-ecr-image.html
 ### Airflow
  - Airflow Variables
     - `s3_bucket` - The same bucket used with the provided notebook for inputs and outputs. (The value assigned to the `default_bucket` variable in the notebook)
-    - `model_package_group` - The name of the Sagemaker Model Registry Group used
- - AWS environment variables in your .env 
-   - AWS_ACCESS_KEY_ID=<your aws access key id>
-   - AWS_SECRET_ACCESS_KEY=<your aws secret access key> 
-   - AWS_SESSION_TOKEN (If you are using a personal AWS credentials)
-   - AWS_DEFAULT_REGION=<your aws region>
-   - AIRFLOW__CORE__XCOM_BACKEND=include.s3_xcom_backend.S3XComBackend
+    - `model_package_group` - The name of the Sagemaker Model Registry Group that you created
+    - `role_arn` - Role ARN that to be used with the Processing Job task. Usually your Sagemaker execution Role Arn.
+ - Airflow Connections
+   - `aws_default` - [AWS](https://airflow.apache.org/docs/apache-airflow-providers-amazon/stable/connections/aws.html)
---
+++ dags/sagemaker-batch-inference-processing.py
@@ -0,0 +1,133 @@
+from datetime import datetime, timedelta
+
+import boto3
+# import pandas as pd
+from airflow.decorators import dag, task, task_group
+from airflow.providers.amazon.aws.operators.sagemaker import SageMakerTransformOperator
+from airflow.providers.amazon.aws.operators.sagemaker import SageMakerProcessingOperator
+from airflow.providers.amazon.aws.hooks.sagemaker import SageMakerHook
+
+output_s3_key = "experiments-demo/predict/output/"
+raw_data = "experiments-demo/validation.csv"
+clean_data = "experiments-demo/predict/input/clean_data.csv"
+
+
+@dag(
+    start_date=datetime(2022, 8, 4),  # Best practice is to use a static start_date.
+    max_active_runs=1,
+    schedule_interval=None,
+    # Default settings applied to all tasks within the DAG; can be overwritten at the task level.
+    default_args={
+        "retries": 0,
+        "retry_delay": timedelta(minutes=3),
+        'aws_conn_id': 'aws_default'
+    },
+    # default_view="grid",
+    catchup=False,
+    tags=["example", "aws", "sagemaker"],
+)
+def sagemaker_batch_inference_processing():
+    """
+    This demonstrates a batch inference pipeline using Amazon Sagemaker's Model Registry, Batch Transform, and Processing
+    features.
+    """
+
+    process_data = SageMakerProcessingOperator(
+        task_id='process_data',
+        config={
+            "ProcessingJobName": 'astronomer-blogpost-batch-infer-{}'.format("{{ ts_nodash }}"),
+            "ProcessingInputs": [
+                {
+                    "InputName": "input",
+                    "AppManaged": False,
+                    "S3Input": {
+                        "S3Uri": 's3://{0}/{1}'.format("{{ var.value.s3_bucket }}", raw_data),
+                        "LocalPath": "/opt/ml/processing/input",
+                        "S3DataType": "S3Prefix",
+                        "S3InputMode": "File",
+                        "S3DataDistributionType": "FullyReplicated",
+                        "S3CompressionType": "None",
+                    },
+                },
+            ],
+            "ProcessingOutputConfig": {
+                "Outputs": [
+                    {
+                        "OutputName": "output",
+                        "S3Output": {
+                            "S3Uri": 's3://{0}/{1}'.format("{{ var.value.s3_bucket }}", clean_data),
+                            "LocalPath": "/opt/ml/processing/output",
+                            "S3UploadMode": "EndOfJob",
+                        },
+                        "AppManaged": False,
+                    }
+                ]
+            },
+            "ProcessingResources": {
+                "ClusterConfig": {
+                    'InstanceCount': 1,
+                    'InstanceType': 'ml.t3.medium',
+                    'VolumeSizeInGB': 1,
+                }
+            },
+            "StoppingCondition": {"MaxRuntimeInSeconds": 300},
+            "AppSpecification": {
+                "ImageUri": "{{ var.value.ecr_repository_uri }}",
+            },
+            "RoleArn": "{{ var.value.role_arn }}",
+        }
+    )
+
+    @task(task_id='get_latest_model_version')
+    def get_latest_model_version(mpg):
+        sagemaker_hook = SageMakerHook()
+
+        sagemaker_client = sagemaker_hook.get_conn()
+
+        # sagemaker_client = boto3.client('sagemaker')
+        registered_models = sagemaker_client.list_model_packages(
+            ModelPackageGroupName=mpg
+        )
+
+        model_version = sagemaker_client.describe_model_package(
+            ModelPackageName=registered_models['ModelPackageSummaryList'][0]['ModelPackageArn']
+        )
+
+        model_name = "astronomer-blogpost-v{}".format(model_version['ModelPackageVersion'])
+
+        return model_name
+
+    predict = SageMakerTransformOperator(
+        task_id='predict',
+        config={
+            "DataProcessing": {
+                "JoinSource": "Input",
+            },
+            "TransformJobName": "astronomer-blogpost-job-{0}".format("{{ ts_nodash }}"),
+            "TransformInput": {
+                "DataSource": {
+                    "S3DataSource": {
+                        "S3DataType": "S3Prefix",
+                        "S3Uri": "s3://{0}/{1}".format("{{ var.value.s3_bucket }}", clean_data)
+                    }
+                },
+                "ContentType": "text/csv",
+                "SplitType": "Line"
+            },
+            "TransformOutput": {
+                "S3OutputPath": "s3://{0}/{1}".format("{{ var.value.s3_bucket }}", output_s3_key),
+                "Accept": "text/csv",
+                "AssembleWith": "Line",
+            },
+            "TransformResources": {
+                "InstanceCount": 1,
+                "InstanceType": "ml.m5.xlarge"
+            },
+            "ModelName": "{{ ti.xcom_pull(task_ids='get_latest_model_version') }}"
+        }
+    )
+
+    [process_data, get_latest_model_version("{{ var.value.model_package_group }}")] >> predict
+
+
+dag = sagemaker_batch_inference_processing()
